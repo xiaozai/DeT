1c3e14d52845d54a780e2c9b62f16658cd48d9ac
xiaozai <ysongonly@gmail.com>
2021-06-13 00:18:24 +0300

    add new RGBD fusion

--- ltr/models/backbone/resnet.py
+++ ltr/models/backbone/resnet.py
@@ -7,6 +7,7 @@ from .base import Backbone
 # from ltr.external.depthconv.functions import depth_conv
 # from ltr.external.depthconv.modules import DepthConv
 # from ltr.external.depthavgpooling.modules import Depthavgpooling
+import torch
 
 def conv3x3(in_planes, out_planes, stride=1, dilation=1):
     """3x3 convolution with padding"""
@@ -60,6 +61,7 @@ class Bottleneck(nn.Module):
 
     def __init__(self, inplanes, planes, stride=1, downsample=None, dilation=1):
         super(Bottleneck, self).__init__()
+
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
@@ -73,7 +75,6 @@ class Bottleneck(nn.Module):
 
     def forward(self, x):
         residual = x
-
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
@@ -221,6 +222,204 @@ class ResNet(Backbone):
 
         raise ValueError('output_layer is wrong.')
 
+class ResNet_RGBD_Fusion(Backbone):
+    """ ResNet network module. Allows extracting specific feature blocks."""
+    def __init__(self, block, layers, output_layers, num_classes=1000, inplanes=64, dilation_factor=1, frozen_layers=(), merge='max'):
+        self.inplanes = inplanes
+        super(ResNet_RGBD_Fusion, self).__init__(frozen_layers=frozen_layers)
+        self.output_layers = output_layers
+        self.merge = merge
+
+        if self.merge == 'conv':
+            self.merge_layers = []
+            self.merge_layers['conv1'] = nn.Conv2d(in_planes*2, in_planes, kernel_size=1, stride=1, bias=False)
+            self.merge_layers['layer1'] = nn.Conv2d(in_planes*4, in_planes*2, kernel_size=1, stride=1, bias=False)
+            self.merge_layers['layer2'] = nn.Conv2d(in_planes*8, in_planes*4, kernel_size=1, stride=1, bias=False)
+            self.merge_layers['layer3'] = nn.Conv2d(in_planes*16, in_planes*8, kernel_size=1, stride=1, bias=False)
+            self.merge_layers['layer4'] = nn.Conv2d(in_planes*32, in_planes*16, kernel_size=1, stride=1, bias=False)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        stride = [1 + (dilation_factor < l) for l in (8, 4, 2)]
+
+        self.conv1_rgb = nn.Conv2d(3, inplanes, kernel_size=7, stride=2, padding=3,bias=False)
+        self.bn1_rgb = nn.BatchNorm2d(inplanes)
+
+        self.conv1_depth = nn.Conv2d(3, inplanes, kernel_size=7, stride=2, padding=3,bias=False)
+        self.bn1_depth = nn.BatchNorm2d(inplanes)
+        self.layer1_rgb, self.layer1_depth = self._make_layer(block, inplanes, layers[0], dilation=max(dilation_factor//8, 1))
+        self.layer2_rgb, self.layer2_depth = self._make_layer(block, inplanes*2, layers[1], stride=stride[0], dilation=max(dilation_factor//4, 1))
+        self.layer3_rgb, self.layer3_depth = self._make_layer(block, inplanes*4, layers[2], stride=stride[1], dilation=max(dilation_factor//2, 1))
+        self.layer4_rgb, self.layer4_depth = self._make_layer(block, inplanes*8, layers[3], stride=stride[2], dilation=dilation_factor)
+
+
+        out_feature_strides = {'conv1': 4, 'layer1': 4, 'layer2': 4*stride[0], 'layer3': 4*stride[0]*stride[1],
+                               'layer4': 4*stride[0]*stride[1]*stride[2]}
+
+        # TODO better way?
+        if isinstance(self.layer1_rgb[0], BasicBlock):
+            out_feature_channels = {'conv1': inplanes, 'layer1': inplanes, 'layer2': inplanes*2, 'layer3': inplanes*4,
+                               'layer4': inplanes*8}
+        elif isinstance(self.layer1_rgb[0], Bottleneck):
+            base_num_channels = 4 * inplanes
+            out_feature_channels = {'conv1': inplanes, 'layer1': base_num_channels, 'layer2': base_num_channels * 2,
+                                    'layer3': base_num_channels * 4, 'layer4': base_num_channels * 8}
+        else:
+            raise Exception('block not supported')
+
+        self._out_feature_strides = out_feature_strides
+        self._out_feature_channels = out_feature_channels
+
+        # self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+
+        self.fc_rgb = nn.Linear(inplanes*8 * block.expansion, num_classes)
+        self.fc_depth = nn.Linear(inplanes*8 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+
+
+    def out_feature_strides(self, layer=None):
+        if layer is None:
+            return self._out_feature_strides
+        else:
+            return self._out_feature_strides[layer]
+
+    def out_feature_channels(self, layer=None):
+        if layer is None:
+            return self._out_feature_channels
+        else:
+            return self._out_feature_channels[layer]
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers_depth = []
+        layers.append(block(self.inplanes, planes, stride, downsample, dilation=dilation))
+        layers_depth.append(block(self.inplanes, planes, stride, downsample, dilation=dilation))
+
+        self.inplanes = planes * block.expansion
+
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+            layers_depth.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers), nn.Sequential(*layers_depth)
+
+    def _add_output_and_check(self, name, x, outputs, output_layers):
+        if name in output_layers:
+            outputs[name] = x
+        return len(output_layers) == len(outputs)
+
+    def _merge(self, x_rgb, x_depth,layer='layer1'):
+        if self.merge == 'max':
+            x_rgb = torch.maximum(x_rgb, x_depth)
+        elif self.merge == 'sum':
+            x_rgb = torch.add(x_rgb, x_depth)
+        elif self.merge == 'multiply':
+            x_rgb = torch.mul(x_rgb, x_depth)
+        elif self.merge == 'conv':
+            if layer == 'fc':
+                x_rgb = (x_rgb + x_depth) / 2
+            else:
+                x_rgb = torch.cat((x_rgb, x_depth), 1)
+                x_rgb = self.merge_layers[layer](x_rgb)
+        else:
+            print('Not such merge type', self.merge)
+        return x_rgb, x_depth
+
+    def forward(self, x, output_layers=None):
+        """ Forward pass with input x. The output_layers specify the feature blocks which must be returned """
+        outputs = OrderedDict()
+
+        if output_layers is None:
+            output_layers = self.output_layers
+
+        x_rgb = x[:, :3, :, :]
+        x_depth = x[:, 3:, :, :]
+
+        x_rgb = self.conv1_rgb(x_rgb)
+        x_rgb = self.bn1_rgb(x_rgb)
+        x_rgb = self.relu(x_rgb)
+
+        x_depth = self.conv1_depth(x_depth)
+        x_depth = self.bn1_depth(x_depth)
+        x_depth = self.relu(x_depth)
+
+        x_rgb, x_depth = self._merge(x_rgb, x_depth, layer='conv1')
+
+        if self._add_output_and_check('conv1', x_rgb, outputs, output_layers):
+            return outputs
+
+        #-----------------------------------------------------------------------
+        x_rgb = self.maxpool(x_rgb)
+        x_depth = self.maxpool(x_depth)
+
+        x_rgb = self.layer1_rgb(x_rgb)
+        x_depth = self.layer1_depth(x_depth)
+        x_rgb, x_depth = self._merge(x_rgb, x_depth, layer='layer1')
+
+        if self._add_output_and_check('layer1', x_rgb, outputs, output_layers):
+            return outputs
+
+        #-----------------------------------------------------------------------
+        x_rgb = self.layer2_rgb(x_rgb)
+        x_depth = self.layer2_depth(x_depth)
+        x_rgb, x_depth = self._merge(x_rgb, x_depth,layer='layer2')
+
+        if self._add_output_and_check('layer2', x_rgb, outputs, output_layers):
+            return outputs
+
+        #-----------------------------------------------------------------------
+        x_rgb = self.layer3_rgb(x_rgb)
+        x_depth = self.layer3_depth(x_depth)
+        x_rgb, x_depth = self._merge(x_rgb, x_depth, layer='layer3')
+
+        if self._add_output_and_check('layer3', x_rgb, outputs, output_layers):
+            return outputs
+
+        #-----------------------------------------------------------------------
+        x_rgb = self.layer4(x_rgb)
+        x_depth = self.layer4(x_depth)
+        x_rgb, x_depth = self._merge(x_rgb, x_depth, layer='layer4')
+
+        if self._add_output_and_check('layer4', x_rgb, outputs, output_layers):
+            return outputs
+
+        #-----------------------------------------------------------------------
+        x_rgb = self.avgpool(x_rgb)
+        x_rgb = x_rgb.view(x_rgb.size(0), -1)
+
+        x_depth = self.avgpool(x_depth)
+        x_depth = x_depth.view(x_depth.size(0), -1)
+
+        x_rgb = self.fc_rgb(x_rgb)
+        x_depth = self.fc_depth(x_depth)
+        x_rgb, x_depth = self._merge(x_rgb, x_depth, layer='fc')
+
+        if self._add_output_and_check('fc', x_rgb, outputs, output_layers):
+            return outputs
+
+        #-----------------------------------------------------------------------
+        if len(output_layers) == 1 and output_layers[0] == 'default':
+            return x_rgb
+
+        raise ValueError('output_layer is wrong.')
+
 
 def resnet_baby(output_layers=None, pretrained=False, inplanes=16, **kwargs):
     """Constructs a ResNet-18 model.
@@ -272,4 +471,80 @@ def resnet50(output_layers=None, pretrained=False, **kwargs):
     if pretrained:
         print('loading pretrained ResNet50 ....')
         model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
+    return model
+
+
+def resnet50_rgbd_fusion(output_layers=None, pretrained=False, pretrained_path=None, merge='max', **kwargs):
+    """Constructs a ResNet-50 model.
+    """
+
+    if output_layers is None:
+        output_layers = ['default']
+    else:
+        for l in output_layers:
+            if l not in ['conv1', 'layer1', 'layer2', 'layer3', 'layer4', 'fc']:
+                raise ValueError('Unknown layer: {}'.format(l))
+
+    model = ResNet_RGBD_Fusion(Bottleneck, [3, 4, 6, 3], output_layers, merge=merge, **kwargs)
+
+
+    if pretrained:
+
+        model_dict = model.state_dict()
+
+
+        print('loading pretrained ResNet50 ....')
+        if pretrained_path is not None:
+            if pretrained_path[0]:
+                pretrained_model_rgb = torch.load(pretrained_path[0])['net']
+                pretrained_model_rgb = {k[18:]: v for k, v in pretrained_model_rgb.items() if k[:17]=='feature_extractor'}
+            else:
+                pretrained_model_rgb = model_zoo.load_url(model_urls['resnet50'])
+
+            if pretrained_path[1]:
+                pretrained_model_depth = torch.load(pretrained_path[1])['net']
+                pretrained_model_depth = {k[18:]: v for k, v in pretrained_model_depth.items() if k[:17]=='feature_extractor'}
+            else:
+                pretrained_model_depth = model_zoo.load_url(model_urls['resnet50'])
+        else:
+            pretrained_model_rgb = model_zoo.load_url(model_urls['resnet50'])
+            pretrained_model_depth = model_zoo.load_url(model_urls['resnet50'])
+
+
+
+        for k, v in model_dict.items():
+            print('model : ', k)
+
+        for k, v in pretrained_model_depth.items():
+            print('pretrained : ', k)
+
+        # # 1. filter out unnecessary keys
+        pretrained_dict_rgb = {}
+        for k, v in pretrained_model_rgb.items():
+            _k = k.split('.')
+            kk = _k[0]+'_rgb'
+            for _kk in _k[1:]:
+                kk += '.'+_kk
+
+            if kk in model_dict:
+                pretrained_dict_rgb[kk] = v
+
+        pretrained_dict_depth = {}
+        for k, v in pretrained_model_depth.items():
+            _k = k.split('.')
+            kk = _k[0]+'_depth'
+            for _kk in _k[1:]:
+                kk += '.'+_kk
+
+            if kk in model_dict:
+                pretrained_dict_depth[kk] = v
+
+        # pretrained_dict_rgb = {k+'_rgb': v for k, v in pretrained_model_rgb.items() if k+'_rgb' in model_dict}
+        # pretrained_dict_depth = {k+'_depth': v for k, v in pretrained_model_depth.items() if k+'_depth' in model_dict}
+        # # 2. overwrite entries in the existing state dict
+        model_dict.update(pretrained_dict_rgb)
+        model_dict.update(pretrained_dict_depth)
+        # # 3. load the new state dict
+        model.load_state_dict(model_dict)
+
     return model
--- pytracking/evaluation/local.py
+++ pytracking/evaluation/local.py
@@ -5,19 +5,19 @@ def local_env_settings():
 
     # Set your local paths here.
 
-    settings.cdtb_path = '/home/yan/Data2/vot-workspace-CDTB/sequences/'
-    settings.cdtb_st_path = '/home/yan/Data2/DOT-results/CDTB-ST/sequences/'
+    settings.cdtb_path = '/home/sgn/Data1/yan/Datasets/CDTB/'
+    settings.cdtb_st_path = '/home/sgn/Data1/yan/Datasets/CDTB-ST'
     settings.davis_dir = ''
-    settings.depthtrack_path = '/home/yan/Data2/vot-workspace-DepthTrack/sequences/'
+    settings.depthtrack_path = '/home/sgn/Data1/yan/Datasets/DepthTrack-v1/test/'
     settings.got10k_path = ''
     settings.got_packed_results_path = ''
     settings.got_reports_path = ''
     settings.lasot_path = ''
-    settings.network_path = '/home/yan/Data2/DeT-models/'    # Where tracking networks are stored.
+    settings.network_path = ''    # Where tracking networks are stored.
     settings.nfs_path = ''
     settings.otb_path = ''
     settings.result_plot_path = '/home/yan/Data2/DeT/pytracking/result_plots/'
-    settings.results_path = '/home/yan/Data2/DOT-results/CDTB-ST/results/'# '/home/yan/Data2/DeT/pytracking/tracking_results/'    # Where to store tracking results
+    settings.results_path = '/home/sgn/Data1/yan/DeT/tracking_results/CDTB-ST/'# '/home/yan/Data2/DeT/pytracking/tracking_results/'    # Where to store tracking results
     settings.segmentation_path = '/home/yan/Data2/DeT/pytracking/segmentation_results/'
     settings.tn_packed_results_path = ''
     settings.tpl_path = ''
