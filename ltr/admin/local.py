ec80060b777f6ef0728bde01a15c7c72689c36e1
xiaozai <ysongonly@gmail.com>
2022-01-11 17:56:43 +0200

    fix test

--- ltr/admin/local.py
+++ ltr/admin/local.py
@@ -1,8 +1,8 @@
 class EnvironmentSettings:
     def __init__(self):
-        self.workspace_dir = '/home/sgn/Data1/yan/DeT-models/'    # Base directory for saving network checkpoints.
+        self.workspace_dir = '/home/yan/Data2/DeT-models/'    # Base directory for saving network checkpoints.
         self.tensorboard_dir = self.workspace_dir + '/tensorboard/'    # Directory for tensorboard files.
-        self.pretrained_networks = self.workspace_dir + '/pretrained_networks/'
+        self.pretrained_networks = self.workspace_dir
         self.lasot_dir = ''
         self.got10k_dir = ''
         self.trackingnet_dir = ''
@@ -17,6 +17,7 @@ class EnvironmentSettings:
         self.davis_dir = ''
         self.youtubevos_dir = ''
         self.cdtb_dir = ''
-        self.depthtrack_dir = '/home/sgn/Data1/yan/Datasets/DeTrack-v1/train_annotated/'
-        self.lasotdepth_dir = '/home/sgn/Data1/yan/Datasets/EstimatedDepth/LaSOT/'
-        self.cocodepth_dir = '/home/sgn/Data1/yan/Datasets/EstimatedDepth/COCO/'
+        self.depthtrack_dir = '/home/yan/Data4/Datasets/DepthTrack/train/'
+        self.trackingnetdepth_dir = '/home/yan/Data4/Datasets/EstimatedDepth/TrackingNet/'
+        self.lasotdepth_dir = '/home/yan/Data4/Datasets/EstimatedDepth/LaSOT/'
+        self.cocodepth_dir = '/home/yan/Data4/Datasets/EstimatedDepth/COCO/'
